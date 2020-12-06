0911ab2384f7b0b2f3ef49ba87c16110a1416eb2
Tristan <tehosken1@gmail.com>
2020-12-06 18:07:12 +0000

    block all public access to buckets for logging and static website source

--- terraform/modules/static_origin/main.tf
+++ terraform/modules/static_origin/main.tf
@@ -12,6 +12,14 @@ resource "aws_s3_bucket" "static_website" {
   tags = merge(var.tags, {})
 }
 
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket                  = aws_s3_bucket.static_website.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "static_website" {
   bucket = aws_s3_bucket.static_website.id
   policy = data.aws_iam_policy_document.s3_policy.json
